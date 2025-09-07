be328eec76fdb25c3e52e7fe025fe73924a0a2d4
notnotnotswipez <strubb06@gmail.com>
2025-09-07 16:25:05 -0400

    changes

---
+++ script.js
@@ -0,0 +1,76 @@
+var namesElement = document.getElementById("projectNames");
+var backgroundTextElement = document.getElementById("backgroundText");
+var changelogElement = document.getElementById("changelog");
+var centerTitleElement = document.getElementById("centerTitle");
+var downloadElement = document.getElementById("downloadButton");
+var versionElement = document.getElementById("version");
+var lastUpdatedElement = document.getElementById("lastUpdated");
+var centerElement = document.getElementById("center");
+
+createEntry("TIMELINE", "timeline.txt");
+createEntry("BL FULL BODY TRACKING", "fullbody.txt");
+
+loadTextSettings("timeline.txt");
+
+repeatBackgroundText("LIGHTS CAMERA ACTION");
+
+
+function loadTextSettings(fileName){
+fetch(fileName)
+  .then((res) => res.text())
+  .then((text) => {
+    let split = text.split("\n");
+    versionElement.textContent = split[0];
+    lastUpdatedElement.textContent = split[1];
+    repeatBackgroundText(split[2]);
+    centerTitleElement.style.color = split[3];
+    downloadElement.style.background = split[3];
+    versionElement.style.color = split[3];
+    centerElement.style.boxShadow = "5px 5px 1px " + split[3];
+
+    let changelogTotal = "";
+    for (let i = 4; i < split.length; i++){
+        changelogTotal += split[i] + "\n";
+    }
+
+    changelogElement.textContent = changelogTotal;
+   })
+  .catch((e) => console.error(e));
+
+}
+
+function createEntry(name, fileName) {
+    var newElement = document.createElement("div");
+    newElement.textContent = name;
+    newElement.className = "projectEntry";
+
+    namesElement.appendChild(newElement);
+    
+    newElement.addEventListener('mouseenter', function() {
+        console.log(name);
+        newElement.style.color = "gray";
+    });
+
+    newElement.addEventListener('mouseleave', function() {
+        console.log(name);
+        newElement.style.color = "white";
+    });
+
+    newElement.addEventListener('click', function() {
+        loadTextSettings(fileName);
+        centerTitleElement.textContent = name;
+    });
+}
+
+function repeatBackgroundText(repeat){
+    repeat += " ";
+    var text = "";
+    for (let i = 0; i < 100; i++){
+        for (let j = 0; j < 15; j++){
+            text += repeat;
+        }
+        text += "\n" + repeat.split(" ")[i % repeat.split(" ").length]+" ";
+    }
+
+    backgroundTextElement.textContent = text;
+}
